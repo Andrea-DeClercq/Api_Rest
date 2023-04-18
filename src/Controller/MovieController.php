a0a86e4018d3eb821ddff9496b3853f5ce17c877
AndreaDC <andrea.dclercq@gmail.com>
2023-04-18 14:30:39 +0200

    ajout fixtures et get all movies

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,17 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/api', name:'api_')]
 class MovieController extends AbstractController
 {
-    #[Route('/movie', name: 'app_movie')]
+    public function __construct(
+        public EntityManagerInterface $em
+    )
+    {
+        
+    }
+
+    #[Route('/welcome', name: 'welcome')]
     public function index(): Response
     {
-        return $this->render('movie/index.html.twig', [
-            'controller_name' => 'MovieController',
+        return $this->json([
+            'message' => 'Bienvenue sur l\'API DigitalMovie',
+            'path' => 'src/Controller/MovieController.php'
         ]);
+    }
+
+    #[Route('/movies', name:"movies")]
+    public function getMovies(): Response
+    {
+        $movies = $this->em->getRepository(Movie::class)->findAll();
+
+        return $this->json($movies);
     }
 }
--- src/DataFixtures/MovieFixtures.php
+++ src/DataFixtures/MovieFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Movie;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,17 @@ class MovieFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+        $faker = \Faker\Factory::create('fr_FR');
+
+        for ($i = 0; $i < 20; $i++)
+        {
+            $movie = new Movie();
+            $movie->setTitle($faker->sentence());
+            $movie->setAuthor($faker->name());
+            $movie->setDescription($faker->paragraph());
+            $manager->persist($movie);
+        }
 
         $manager->flush();
     }
